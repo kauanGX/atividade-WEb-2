e70eaab001ed8d98e684d9f6a14a84b96ba93135
kauanGX <kjens@discente.ifpe.edu.br>
2025-04-30 15:39:40 -0300

    ola

---
+++ atividade_2/database/factories/PublisherFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Publisher;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Publisher>
+ */
+class PublisherFactory extends Factory
+{
+    protected $model = Publisher::class;
+    
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->company, 
+            'address' => $this->faker->address,
+        ];
+    }
+}
